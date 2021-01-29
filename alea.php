a53b7dbef28c3ace71c6fb71b8356891a70e6d70
Sylvie-B <unsolo@hotmail.fr>
2021-01-29 09:15:09 +0100

    test first

--- alea.php
+++ alea.php
@@ -1,11 +1,11 @@
 <?php
 
-$x = $_POST['x'];
-$y = $_POST['y'];
-
 if(!isset($_POST['x'],$_POST['y'])){
     header('location: index.php?error=1');
 }
+
+$x = $_POST['x'];
+$y = $_POST['y'];
 
 if($x >= 0 && $y >= 0 && $x < getrandmax() && $y < getrandmax()){
     $random = rand($x, $y);
